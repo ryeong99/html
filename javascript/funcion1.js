c7d90b44ade096530415c9394c95d473527c9811
ryeong99 <lovolvo1015@gmail.com>
2024-06-18 09:20:40 +0900

    dfasdfe

---
+++ javascript/funcion1.js
@@ -0,0 +1,18 @@
+function add(x, y) {
+    return x + y;
+}
+
+const result = add(10, 20);
+console.log(result);
+
+function add(x, y, z) {
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+console.log(x);
+
+add(10, 20, 30);
+
+var x = 50;
